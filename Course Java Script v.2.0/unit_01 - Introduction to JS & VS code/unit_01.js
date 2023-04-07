3cd8570870ca190b37d038331c0ff809efdddd37
YAROSLAV <ykarpash@gmail.com>
2023-04-07 16:56:23 +0300

    Update unit_01.js

--- Course Java Script v.2.0/unit_01 - Introduction to JS & VS code/unit_01.js
+++ Course Java Script v.2.0/unit_01 - Introduction to JS & VS code/unit_01.js
@@ -8,29 +8,23 @@ console.log('Yaroslav');
 
 console.log(8);
 
-
-
 // Task 3.
 // Выведите в консоль строку: 'Добро '+'пожаловать '+' на курс'
 
 console.log('Ласкаво ' + 'просимо ' + 'на курс');
 
-
 // Task 4.
 // С помощью alert выведите число 2019. После срабатывания кода - закомментируйте его, чтобы не мешал для дальнейшей разработки
 //alert(2019);
-
 
 // Task 5.
 // С помощью console.log выведите результат операции 2019 - 200. Проверьте результат в консоли.
 
 console.log(2019 - 200);
 
-
 // Task 6.
 // На странице создан div#six. С помощью document.getElementById запишите в div#six текст 'Hello World'.
 document.getElementById('six').innerHTML = 'Hello world';
-
 
 // Task 7.
 // На странице создан div#seven. С помощью document.getElementById запишите в данный элемент результат умножения 12 на 12. Умножение можно сделать с помощью знака звездочки ( shift + 8).
@@ -47,7 +41,6 @@ document.querySelector('.out-8').innerHTML = 'task-8';
 // Т.е. результат должен выглядеть <h2>Hello <span class="out-8">world</span></h2>
 document.querySelector('span').innerHTML = 'world';
 
-
 // Task 10.
 //На странице создан div.out-10. С помощью innerHTML запишите внутрь него следующую строку '<h2>Hi</h2>'.
 document.querySelector('.out-10').innerHTML = '<h2>Hi</h2>';
@@ -58,15 +51,11 @@ document.querySelector('.out-10').innerHTML = '<h2>Hi</h2>';
 document.querySelector('.out-11').innerHTML = '123';
 document.querySelector('.out-11').innerHTML += '345';
 
-
-
-
 // Task 12.
 //  На странице создан div.out-12.  Получите его в переменную a. С помощью innerHTML запишите внутрь переменной a число 3.1415. Изучите как в JS пишутся дроби - через точку или через запятую!
 
 let a = document.querySelector('.out-12');
 a.innerHTML = 3.1415;
-
 
 // Task 13.
 // На странице создан div.out-13. Получите его в переменную out13. С помощью innerHTML запишите в него строку: '<img src="https://itgid.info/img/js20/js20_unit_01.png" alt="js">'
@@ -87,13 +76,11 @@ let y1 = 6;
 let y2 = 3;
 document.querySelector('.out-15').innerHTML = y1 / y2;
 
-
 // Task 16.
 // Создайте две переменные x1='Hello', x2 = 5.  В div.out-16 выведите сумму x1 + x2. Изучите результат операции.
 let x1 = 'Hello';
 let x2 = 5;
 document.querySelector('.out-16').innerHTML = x1 + x2;
-
 
 // Task 17.
 // Получите div.out-17 в переменную out17. Выведите эту переменную в консоль. Изучите вывод.
@@ -109,15 +96,12 @@ console.log(out18);
 out18 = 5;
 console.log(out18);
 
-
-
 // Task 19.
 // Получите div.out-19 в переменную out19. Выведите в консоль out19. Теперь получите в эту же переменную out19 блок div c классом out-19-test. Выведите переменную out19 в консоль. Изучите что изменилось.
 let out19 = document.querySelector('.out-19');
 console.log(out19);
 out19 = document.querySelector('.out-19-test');
 console.log(out19);
-
 
 // Task 20
 // Получите div.out-20 в переменную out20. C помощью textContent запишите в него строку '<h2>Hi</h2>'. Изучите результат. Посмотрите как этот вывод отличается от вывода в 10 task.
