a3f3e64d0cbd1ff118789e58514a2595b900e2da
Yaroslav Karpash <ykarpash@gmail.com>
2023-05-07 23:33:43 +0300

    Update unit_10.js

--- Course Java Script v.2.0/unit_10 - Array/unit_10.js
+++ Course Java Script v.2.0/unit_10 - Array/unit_10.js
@@ -332,13 +332,13 @@ document.querySelector('.b-16').onclick = f16;
 let out17 = document.querySelector('.out-17');
 let ar17 = [3, 0, 2, 6, 0, 1, 3, 1, 9, 0, 2, 0];
 function f17() {
-    let out = '';
+    let count = 0;
     for (let i = 0; i < ar17.length; i++) {
         if (ar17[i] > 3) {
-            out += ar17[i];
+            count++;
         }
     }
-    out17.innerHTML = out.length;
+    out17.innerHTML = count;
 }
 document.querySelector('.b-17').onclick = f17;
 
