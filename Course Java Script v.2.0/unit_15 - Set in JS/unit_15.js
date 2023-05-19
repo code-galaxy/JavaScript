7b09f6e526cb5b6d4d995de59c1a278360f4bbac
Yaroslav Karpash <ykarpash@gmail.com>
2023-05-19 14:11:49 +0300

    Update unit_15.js

--- Course Java Script v.2.0/unit_15 - Set in JS/unit_15.js
+++ Course Java Script v.2.0/unit_15 - Set in JS/unit_15.js
@@ -96,8 +96,6 @@ document.querySelector('.b-7').onclick = f7;
 let s8 = new Set([1, 2, 3, 4, 5, 3, 4, 7, 9, 5, 7, 8, 9, 23, 45, 5, 2, 4, 5, 3, 24, 5, 2, 4, 56, 4, 3, 2, 335, 2, 23, 41, 3, 4, 1, 1, 4, 2, 2, 4, 5, 24, 5, 3, 22, 56]);
 let ar8 = [];
 
-console.log(s8);
-
 const f8 = () => {
     let a8 = new Set(s8);
     for (let item of a8) {
@@ -107,9 +105,8 @@ const f8 = () => {
     }
     console.log(ar8);
 }
-
-
 document.querySelector('.b-8').onclick = f8;
+
 
 // Task 9
 //  При нажатии b-9 выполняете функцию f9. Функция должна принимать набор our_set в качестве параметра, преобразовывать его в строку, 
