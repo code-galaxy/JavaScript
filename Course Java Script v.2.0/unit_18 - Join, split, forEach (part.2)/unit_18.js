d9edebde45e35f98da18526e98153ae1eb3f96d4
Yaroslav Karpash <ykarpash@gmail.com>
2023-05-23 11:57:17 +0300

    Update unit_18.js

--- Course Java Script v.2.0/unit_18 - Join, split, forEach (part.2)/unit_18.js
+++ Course Java Script v.2.0/unit_18 - Join, split, forEach (part.2)/unit_18.js
@@ -32,13 +32,11 @@ document.querySelector('.b-2').onclick = t2;
 // Task 3 
 /*  Дан массив a3 = [2, 'hello', 3, 'hi', 4, 'Mazai'] - с помощью forEach переберите массив и создайте новый массив a3_res куда добавьте элементы данного массива являющиеся числом. Запускаться решение должно при вызове функции t3.*/
 
-function t3() {
     let a3 = [2, 'hello', 3, 'hi', 4, 'Mazai'];
-
+function t3() {
     a3.forEach(elem => {
-        if (typeof elem == typeof Number()) a3_res.push(elem);
+        if (typeof elem === 'number') a3_res.push(elem);
     });
-
     document.querySelector('.out-3').innerHTML = a3_res;
 }
 document.querySelector('.b-3').onclick = t3;
